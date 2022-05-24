8fedbd64169c69a736f6f557d1cb4c229132b8c5
Deva16112012 <100427772+Deva16112012@users.noreply.github.com>
2022-05-24 11:55:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,32 +30,56 @@ function my_keydown(e)
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
 			//write a code to check the type of key pressed
 			aplhabetkey();
-			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+			document.getElementById("d1").innerHTML="*You pressed Alphabet Key";
 			console.log("alphabet key");
-		}
 
+		}
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+	if((keyPressed >=48 && keyPressed<=57)){
+		//write a code to check the type of key pressed
+		numberkey();
+		document.getElementById("d1").innerHTML="*You pressed Number Key";
+		console.log("number key");
+
+	}
+	if((keyPressed >=37 && keyPressed<=40)){
+		//write a code to check the type of key pressed
+		arrowkey();
+		document.getElementById("d1").innerHTML="*You pressed Arrow Key";
+		console.log("arrow key");
+
+	}
+	if((keyPressed == 17 && keyPressed == 18) || (keyPressed == 27)){
+		//write a code to check the type of key pressed
+		specialkey();
+		document.getElementById("d1").innerHTML="*You pressed Special Key";
+		console.log("special key");
+
 	}
 }
 
 function aplhabetkey()
 {
-	//upload respective image with the message. 
     img_image="Alpkey.png";
 	add();
 }
 function numberkey()
 {
-	
+    img_image="numkey.png";
+    add();	
 }
 function arrowkey()
 {
+	img_image="Arrkey.png";
+	add();
 }
 function specialkey()
 {
-	
+	img_image="spkey.png";
+	add();	
 }
 function otherkey()
 {
